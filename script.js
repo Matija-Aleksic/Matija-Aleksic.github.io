9f72748c3af680d42f46c217e9491d74966abf3c
Matija Aleksic <matijaaleksic22@gmail.com>
2025-01-22 13:23:10 +0100

    code cleanup

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerBtn = document.querySelector(".hamburger-btn");
-    const mobileNav = document.querySelector(".mobile-nav");
-    const mobileArtistContainer = document.querySelector(".artist-container");
+  const hamburgerBtn = document.querySelector(".hamburger-btn");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const mobileArtistContainer = document.querySelector(".artist-container");
+  const eventSchedule = document.querySelector(".event-schedule");
+  const spotlight = document.querySelector(".spotlight");
 
-    hamburgerBtn.addEventListener("click", function () {
-        mobileNav.classList.toggle("open");
-        if (mobileArtistContainer) {
-            mobileArtistContainer.classList.toggle("close");
-        }
-    });
+  hamburgerBtn.addEventListener("click", function () {
+    mobileNav.classList.toggle("open");
+    if (mobileArtistContainer) {
+      mobileArtistContainer.classList.toggle("close");
+    }
+    if (eventSchedule) {
+      eventSchedule.classList.toggle("close");
+    }
+    if (spotlight) {
+      spotlight.classList.toggle("close");
+    }
+  });
 });
