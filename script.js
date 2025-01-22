9bcdaf8d7ce705d4f70091e9ee0c3a16f9ee25e2
Matija Aleksic <matijaaleksic22@gmail.com>
2025-01-22 11:15:19 +0100

    implemented mobile for artists and artist spotlight

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburgerBtn = document.querySelector(".hamburger-btn");
     const mobileNav = document.querySelector(".mobile-nav");
+    const mobileArtistContainer = document.querySelector(".artist-container");
 
     hamburgerBtn.addEventListener("click", function () {
         mobileNav.classList.toggle("open");
+        if (mobileArtistContainer) {
+            mobileArtistContainer.classList.toggle("close");
+        }
     });
 });
